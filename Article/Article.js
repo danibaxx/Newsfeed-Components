ecd360b6fa006699ab0316e321f1f8ecafd8cb5a
Danielle B <danibaxx@hotmail.com>
2019-05-07 19:36:10 -0600

    updated menu.js, updated article.js has functionality, updated index.less with imports

--- Article/Article.js
+++ Article/Article.js
@@ -3,27 +3,36 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.domElement = domElement;
+    // console.log(domElement);
+
+   // create a reference to the ".expandButton" class.  
+    this.expandButton = this.domElement.querySelector('.expandButton');
+    // console.log(this.expandButton);
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.innerHTML = 'expand';
+
+// Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
 
+
+// Using our reference to the domElement, toggle a class to expand or hide the article.
+this.domElement.classList.toggle('article-open');
+this.domElement.classList.toggle('close');
   }
 }
 
-/* START HERE: 
+//  START HERE: 
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+// - Select all classes named ".article" and assign that value to the articles variable. 
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
-*/
+// - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
-let articles;
+let articles = document.querySelectorAll('.article').forEach(article => {
+  new Article(article);
+});
